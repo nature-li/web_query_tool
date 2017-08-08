3dced8181bafffa833389f856dbbcde3ca156252
liyanguo <lyg@meitu.com>
2017-08-08 19:11:34 +0800

    添加导航功能

---
+++ static/js/nav/hive_index.js
@@ -0,0 +1,43 @@
+// frame改变大小
+function frame_auto_size() {
+    var newHeight = $("#right_frame").contents().find('body').height();
+    if (newHeight < 600) {
+        newHeight = 600;
+    }
+    $("#left_frame_col").height(newHeight);
+    $("#right_frame_col").height(newHeight);
+}
+
+// frame 加载时自适应大小
+$("#right_frame").load(function () {
+    console.log("13");
+    frame_auto_size();
+});
+
+// a_day_count 点击事件
+$(document).on("click", "#a_day_count", function () {
+    $("#right_frame").attr("src", "day_count");
+    // 自适应框架大小
+    frame_auto_size();
+});
+
+// a_hour_count 点击事件
+$(document).on("click", "#a_hour_count", function () {
+    $("#right_frame").attr("src", "hour_count");
+    // 自适应框架大小
+    frame_auto_size();
+});
+
+// a_user_control 点击事件
+$(document).on("click", "#a_user_control", function () {
+    $("#right_frame").attr("src", "user_list");
+    // 自适应框架大小
+    frame_auto_size();
+});
+
+// a_ad_network_id_control 点击事件
+$(document).on("click", "#a_network_control", function () {
+    $("#right_frame").attr("src", "network_list");
+    // 自适应框架大小
+    frame_auto_size();
+});
--- static/js/nav/index.js
+++ static/js/nav/index.js
@@ -1,60 +1,15 @@
-// frame改变大小
-function frame_auto_size() {
-    var newHeight = $("#right_frame").contents().find('body').height();
-    if (newHeight < 600) {
-        newHeight = 600;
-    }
-    $("#left_frame_col").height(newHeight);
-    $("#right_frame_col").height(newHeight);
-}
+// nav_hive_ref_a 点击事件
+$("#nav_hive_ref_a").click(function () {
+    // 清空侧边栏
+    $("#main-nav").html("");
 
-// frame 加载时自适应大小
-$("#right_frame").load(function () {
-    frame_auto_size();
-});
+    // 添加侧边栏
+    $("#main-nav").append('<li><a href="#systemViewer" class="nav-header" data-toggle="collapse" aria-expanded="true"><i class="glyphicon glyphicon-th-list"></i>hive统计<span class="pull-right glyphicon glyphicon-chevron-down"></span></a><ul id="systemViewer" class="nav nav-list collapse secondmenu in" aria-expanded="true" style="height: 80px;"><li><a id="a_day_count" href="#">日统计<i id="i_day_count" class="glyphicon glyphicon-eye-open"></i></a></li><li><a id="a_hour_count" href="#">时统计<i id="i_hour_count" class="glyphicon glyphicon-eye-open"></i></a></li></ul></li><li><a href="#systemControl" class="nav-header collapse" data-toggle="collapse"><i class="glyphicon glyphicon-th-list"></i>系统管理<span class="pull-right glyphicon glyphicon-chevron-down"></span></a><ul id="systemControl" class="nav nav-list collapse secondmenu" aria-expanded="true" style="height: 0px;"><li><a id="a_user_control" href="#">用户管理<i id="i_user_control" class="glyphicon glyphicon-pencil"></i></a></li><li><a id="a_network_control" href="#">渠道管理<i id="i_network_control" class="glyphicon glyphicon-pencil"></i></a></li></ul></li></ul>');
 
-// a_day_count 点击事件
-$("#a_day_count").click(function () {
-    //$("#i_day_count").attr('class', 'glyphicon glyphicon-eye-open');
-    //$("#i_hour_count").attr('class', '');
-    //$("#i_user_control").attr('class', '');
-    //$("#i_ad_network_id_control").attr('class', '');
+    // 加载数据
     $("#right_frame").attr("src", "day_count");
+
     // 自适应框架大小
     frame_auto_size();
 });
-
-// a_hour_count 点击事件
-$("#a_hour_count").click(function () {
-    //$("#i_day_count").attr('class', '');
-    //$("#i_hour_count").attr('class', 'glyphicon glyphicon-eye-open');
-    //$("#i_user_control").attr('class', '');
-    //$("#i_ad_network_id_control").attr('class', '');
-    $("#right_frame").attr("src", "hour_count");
-    // 自适应框架大小
-    frame_auto_size();
-});
-
-// a_user_control 点击事件
-$("#a_user_control").click(function () {
-    //$("#i_day_count").attr('class', '');
-    //$("#i_hour_count").attr('class', '');
-    //$("#i_user_control").attr('class', 'glyphicon glyphicon-pencil');
-    //$("#i_network_control").attr('class', '');
-    $("#right_frame").attr("src", "user_list");
-    // 自适应框架大小
-    frame_auto_size();
-});
-
-// a_ad_network_id_control 点击事件
-$("#a_network_control").click(function () {
-    //$("#i_day_count").attr('class', '');
-    //$("#i_hour_count").attr('class', '');
-    //$("#i_user_control").attr('class', '');
-    //$("#i_network_control").attr('class', 'glyphicon glyphicon-pencil');
-    $("#right_frame").attr("src", "network_list");
-    // 自适应框架大小
-    frame_auto_size();
-});
-
 
