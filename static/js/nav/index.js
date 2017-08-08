06a2f66320045bd532c369518fe080d82ef1fa7b
liyanguo <lyg@meitu.com>
2017-08-08 20:01:37 +0800

    不再展开侧边栏

--- static/js/nav/index.js
+++ static/js/nav/index.js
@@ -9,9 +9,9 @@ function init_main_page() {
 
     // 添加侧边栏
     var html = `
-    <li><a href="#systemViewer" class="nav-header" data-toggle="collapse" aria-expanded="true"><i
+    <li><a href="#systemViewer" class="nav-header collapse" data-toggle="collapse" aria-expanded="true"><i
             class="glyphicon glyphicon-th-list"></i>hive统计<span class="pull-right glyphicon glyphicon-chevron-down"></span></a>
-        <ul id="systemViewer" class="nav nav-list collapse secondmenu in" aria-expanded="true" style="height: 80px;">
+        <ul id="systemViewer" class="nav nav-list collapse secondmenu" aria-expanded="true"">
             <li><a id="a_day_count" href="#">日统计<i id="i_day_count" class="glyphicon glyphicon-eye-open"></i></a></li>
             <li><a id="a_hour_count" href="#">时统计<i id="i_hour_count" class="glyphicon glyphicon-eye-open"></i></a></li>
         </ul>
@@ -46,9 +46,9 @@ $("#nav_user_ref_a").click(function () {
 
     // 添加侧边栏
     var html = `
-    <li><a href="#systemViewer" class="nav-header" data-toggle="collapse" aria-expanded="true"><i
+    <li><a href="#systemViewer" class="nav-header collapse" data-toggle="collapse" aria-expanded="true"><i
         class="glyphicon glyphicon-th-list"></i>系统管理<span class="pull-right glyphicon glyphicon-chevron-down"></span></a>
-    <ul id="systemViewer" class="nav nav-list collapse secondmenu in" aria-expanded="true" style="height: 80px;">
+    <ul id="systemViewer" class="nav nav-list collapse secondmenu" aria-expanded="true"">
         <li><a id="a_user_control" href="#">用户管理<i id="i_user_control" class="glyphicon glyphicon-pencil"></i></a></li>
     </ul>
     </li>`;
